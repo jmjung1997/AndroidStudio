5d6325375131a3a4a412bb197027b153e8c0851c
Jeong jaemin <jmjung1997@gmail.com>
2022-04-24 23:04:00 +0900

    final

--- sixthweek/practice7_2/app/src/main/java/com/example/practice7_2/MainActivity.java
+++ sixthweek/practice7_2/app/src/main/java/com/example/practice7_2/MainActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
-import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -19,28 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("배경색 바꾸기(컨텍스트 메뉴)");
+        setTitle("배경색바꾸기(컨텍스트 메뉴활용)");
         baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
         button1 = (Button) findViewById(R.id.button1);
-        registerForContextMenu(button1);
         button2 = (Button) findViewById(R.id.button2);
+        registerForContextMenu(button1);
         registerForContextMenu(button2);
+    }
 
-        public void onCreateContextMenu (ContextMenu menu, View v, ContextMenuInfo menuInfo)
-        {
-            super.onCreateContextMenu(menu, v, menuInfo);
-            MenuInflater mInflater = getMenuInflater();
-            if (v == button1) {
-                menu.setHeaderTitle("배경색 변경");
-                menu.setHeaderIcon(R.drawble.icon1);
-                mInflater.inflate(R.menu.menu1, menu);
-            }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
 
-            if (v == button2) {
-                mInflater.inflate(R.menu.menu2, menu);
-            }
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater mi = getMenuInflater();
+        if (v == button1) {
+            menu.setHeaderTitle("배경색 변경");
+            mi.inflate(R.menu.menu1, menu);
+        }
+        if (v == button2) {
+            mi.inflate(R.menu.menu2, menu);
         }
     }
 }
+
+
 
 
